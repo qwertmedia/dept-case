c2b7bbd9b4ec03ab56dbbad1569fdeb492ad49fa
qwertmedia <jarmovdh@gmail.com>
2022-06-24 12:12:46 +0200

    motion

--- src/components/HeroBanner/HeroBanner.js
+++ src/components/HeroBanner/HeroBanner.js
@@ -1,4 +1,6 @@
 import React from "react";
+import { motion } from "framer-motion"
+
 import "./HeroBanner.scss";
 import Banner from "./assets/herobanner.jpg";
 
@@ -6,13 +8,13 @@ const HeroBanner = () => {
   return (
     <div className="banner">
       <img className="banner-img" src={Banner} alt="" />
-      <div className="info">
+      <motion.div whileHover={{scale:1.03,}} className="info">
         <h1>WORK</h1>
         <h2>
           A selection of projects that <b>pioneer</b> <b>tech</b> and{" "}
           <b>marketing</b> to help brands stay ahead.
         </h2>
-      </div>
+      </motion.div>
     </div>
   );
 };
